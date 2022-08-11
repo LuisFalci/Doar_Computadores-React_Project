16f0b46f824beacc10b5e6cffc1747a0f5d520cf
Luis Gustavo Furtuozo Falci <78694561+LuisFalci@users.noreply.github.com>
2022-08-11 18:24:35 -0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,6 @@ import axios from "axios";
 import { useState, useRef } from "react";
 import Input from "../components/form/Input";
 
-
-
-
 export default function Home() {
   // Quantidade de Aparelhos
   const [deviceQuantity, setDeviceQuantity] = useState(1);
